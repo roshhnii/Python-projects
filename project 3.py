4cff48f325dccf21b72b40b91ef940db255b41da
roshhnii <83349501+roshhnii@users.noreply.github.com>
2021-09-05 23:48:38 +0530

    Add files via upload

---
+++ project 3.py
@@ -0,0 +1,8 @@
+list1 = [12, -7, 5, 64, -14,]
+print("Original numbers in the list: ", list1) 
+new_nums = list(filter (lambda x: x >0, list1)) 
+print("Positive numbers in the list: " ,new_nums)
+list2 = [12, 14, -95, 3,]
+print("Original numbers in the list: ", list2) 
+new__nums = list(filter (lambda x: x >0, list2)) 
+print("Positive numbers in the list: " ,new__nums)
